0e9ab8bdef45333d22790651fc0b73a6e458e63b
bca-rcm-24BCA0036 <Preeti.Dalai@rcm.ac>
2026-04-26 23:33:05 +0530

    Add login function to script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 alert("Welcome to my portfolio!");
+function login() {
+    var user = document.getElementById("username").value; 
+    var pass = document.getElementById("password").value;
+
+    if (user === "preeti" && pass === "1234") {
+        window.location.href = "index.html";
+    } else {
+        document.getElementById("message").innerText = "Wrong username or password";
+    }
+}
+
